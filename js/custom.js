8af7613fb8f96c68b5919e8b20168797ce55a998
rezawp <rezaaawp@gmail.com>
2023-07-16 20:58:55 +0700

    update redirect lang

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,43 @@
-
-  (function ($) {
-  
+(function ($) {
   "use strict";
 
-    // PRE LOADER
-    $(window).load(function(){
-      $('.preloader').fadeOut(1000); // set duration in brackets    
+  fetch("https://rezawp.com/ip.php")
+    .then((res) => res.json())
+    .then((res) => {
+      fetch(`https://ipinfo.io/${res.ip}/json`)
+        .then((res) => res.json())
+        .then((resIp) => {
+          if (resIp.country === "ID") {
+            return location.assign("https://id.rezawp.com");
+          }
+        });
     });
 
-    // CUSTOM LINK
-    $('.custom-link').click(function(){
-    var el = $(this).attr('href');
-    var elWrapped = $(el);
-    var header_height = $('.navbar').height() + 10;
+  // PRE LOADER
+  $(window).load(function () {
+    $(".preloader").fadeOut(1000); // set duration in brackets
+  });
 
-    scrollToDiv(elWrapped,header_height);
+  // CUSTOM LINK
+  $(".custom-link").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height() + 10;
+
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
-  }
-});
-    
-  })(window.jQuery);
-
-
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+})(window.jQuery);
